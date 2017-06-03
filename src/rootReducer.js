7dd66e4a33afe866885da9d2019fa566d93a5533
melih <melihberberoglu@gmail.com>
2017-06-03 22:47:01 +0300

    root reducer added

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,14 +1,22 @@
 import { combineReducers } from "redux";
 import { reducer as reduxformReducer } from "redux-form";
+import BLOG from './containers/Blog/Blog.constants';
 
-import homeReducer from "./containers/Home/Home.reducer";
-
-// import HOME from "./containers/Home/Home.constants";
+import blogReducer from "./containers/Blog/Blog.reducer";
 
 
 const rootReducer = combineReducers({
-  home: homeReducer,
-  form: reduxformReducer,
+  blog: blogReducer,
+  form: reduxformReducer.plugin({
+    commentForm: (state, action) => {
+      switch (action.type) {
+        case BLOG.ADD_COMMENT_SUCCESS:
+          return undefined;
+        default:
+          return state;
+      }
+    }
+  }),
 });
 
 
