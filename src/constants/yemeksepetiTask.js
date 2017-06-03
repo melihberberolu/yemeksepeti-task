3d73bdb83aa833d1894064e2e0966e8273960ad4
melih <melihberberoglu@gmail.com>
2017-06-03 22:46:24 +0300

    yemeksepeti task const added

---
+++ src/constants/yemeksepetiTask.js
@@ -0,0 +1,82 @@
+const data = {
+  "Id": 12,
+  "Title": "Quisque eu est vestibulum!",
+  "Content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam rutrum imperdiet consectetur. Sed ut malesuada tortor. Phasellus ac eros interdum, ultricies lectus ut, suscipit sapien. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Vestibulum enim dolor, vulputate at interdum sed, hendrerit ornare augue. Duis egestas vehicula ultrices. Cras lacinia sodales augue, quis ullamcorper nisi. Praesent et dui lobortis, convallis erat a, mattis tellus. Vestibulum eu sem sit amet elit hendrerit tempus interdum tempor elit. Aliquam fringilla ipsum a elementum sodales. Maecenas lacinia cursus dui non interdum. Nam sagittis fermentum eros, vitae viverra neque euismod at. Vestibulum tempor leo id sollicitudin rhoncus. Phasellus felis nisi, ultrices non molestie eget, cursus eget augue. Ut lobortis iaculis aliquet. Suspendisse et pretium urna. Maecenas placerat quam libero, non imperdiet elit pulvinar vitae. Donec posuere massa vulputate massa suscipit hendrerit. Sed gravida faucibus ex, eget consectetur dolor malesuada at. Suspendisse commodo ex non ipsum aliquam, ac egestas lacus sodales. Nulla quis dolor ac dui bibendum rhoncus. Quisque eu est vestibulum, aliquam risus at, aliquam nibh. Ut semper lobortis massa id interdum. Etiam enim lacus, mollis a mauris et, vestibulum sollicitudin nisi. Aliquam erat volutpat. Nullam magna lectus, euismod vel mollis sit amet, feugiat eget nisl. Etiam mi ex, faucibus nec luctus a, volutpat ut lectus. Integer mattis pulvinar ullamcorper. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque rutrum ante interdum libero imperdiet, in aliquam lectus elementum. Etiam pulvinar sagittis orci, sit amet aliquam neque commodo non. Pellentesque ullamcorper nulla metus, nec pharetra nulla elementum in. Praesent dignissim iaculis lacus in consequat. Aliquam erat volutpat. Curabitur sit amet tincidunt risus, quis blandit risus. Fusce ut metus a tellus luctus volutpat. Nulla in vehicula nibh. Nullam ac erat tristique, sagittis dolor sed, efficitur mi. Integer malesuada erat eget libero congue rutrum. Cras vehicula velit dui, a commodo purus laoreet eget. In accumsan interdum sollicitudin.",
+  "Image": "http://cdn.yemek.com/uploads/2016/12/yilbasi-yeni-lacivert-manset.jpg",
+  "Comments": [
+    {
+      "Id": 1960,
+      "Content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sollicitudin nisl tellus, nec rhoncus dui consequat gravida. Quisque id pretium elit, ut molestie neque.",
+      "Date": "/Date(1440188458000-0000)/",
+      "User": {
+        "DisplayName": "Ahmet",
+        "AvatarImageUrl": "http://cdn.yemek.com/assets/images/profile-default.jpg"
+      }
+    },
+    {
+      "Id": 1843,
+      "Content": "Curabitur ut elit vel elit fermentum sollicitudin. Quisque vitae accumsan tellus. Duis diam arcu, hendrerit sed interdum nec, convallis at enim.",
+      "Date": "/Date(1440188458000-0000)/",
+      "User": {
+        "DisplayName": "Aras",
+        "AvatarImageUrl": "http://cdn.yemek.com/assets/images/profile-default.jpg"
+      }
+    },
+    {
+      "Id": 1456,
+      "Content": "Cras viverra nibh faucibus porttitor venenatis. Nulla nec est ut risus bibendum consectetur et nec erat. Integer dignissim faucibus metus, id aliquet magna interdum eu.",
+      "Date": "/Date(1440188458000-0000)/",
+      "User": {
+        "DisplayName": "Turgut",
+        "AvatarImageUrl": "http://cdn.yemek.com/assets/images/profile-default.jpg"
+      }
+    },
+    {
+      "Id": 1643,
+      "Content": "Mauris aliquet hendrerit tellus, at maximus augue tincidunt ut.",
+      "Date": "/Date(1440188458000-0000)/",
+      "User": {
+        "DisplayName": "Ziya",
+        "AvatarImageUrl": "http://cdn.yemek.com/assets/images/profile-default.jpg"
+      }
+    },
+    {
+      "Id": 1845,
+      "Content": "Morbi ullamcorper, nunc vitae sollicitudin pellentesque, arcu est iaculis ante, mattis vehicula odio odio nec quam.",
+      "Date": "/Date(1440188458000-0000)/",
+      "User": {
+        "DisplayName": "Recai",
+        "AvatarImageUrl": "http://cdn.yemek.com/assets/images/profile-default.jpg"
+      }
+    },
+    {
+      "Id": 1743,
+      "Content": "Donec id justo nec orci vulputate lacinia. Nam diam dui, tincidunt ut nibh laoreet, lacinia condimentum eros.",
+      "Date": "/Date(1440188458000-0000)/",
+      "User": {
+        "DisplayName": "ismail",
+        "AvatarImageUrl": "http://cdn.yemek.com/assets/images/profile-default.jpg"
+      }
+    },
+    {
+      "Id": 1223,
+      "Content": "Donec non justo lorem. Fusce ut volutpat nisi.",
+      "Date": "/Date(1440188458000-0000)/",
+      "User": {
+        "DisplayName": "Mehmet",
+        "AvatarImageUrl": "http://cdn.yemek.com/assets/images/profile-default.jpg"
+      }
+    },
+    {
+      "Id": 1109,
+      "Content": "Maecenas elementum accumsan tellus, a dignissim nibh gravida id. Vivamus purus ex, sagittis ut lorem in, molestie pulvinar justo. Donec sit amet luctus mi, vitae maximus elit. Nulla eget diam ex. Nullam sit amet felis urna. Etiam ut vehicula turpis. Fusce ultricies mauris in semper vehicula. Fusce a tincidunt sapien.",
+      "Date": "/Date(1440188458000-0000)/",
+      "User": {
+        "DisplayName": "Hasan",
+        "AvatarImageUrl": "http://cdn.yemek.com/assets/images/profile-default.jpg"
+      }
+    }
+  ]
+};
+
+export default data;
